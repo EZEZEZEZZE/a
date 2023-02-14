643890ea02e930c30ecc4e87f59aab88a1c399e6
EZEZEZEZZE <104246252+EZEZEZEZZE@users.noreply.github.com>
2023-02-13 23:30:24 -0600

    Update Bedwars.lua

--- CustomModules/Bedwars.lua
+++ CustomModules/Bedwars.lua
@@ -1801,16 +1801,16 @@ runcode(function()
         ["Callback"] = function(Callback)
             Enabled = Callback
 			if Enabled then
-                local obj = game:GetObjects("rbxassetid://11221092608")[1]
+                local obj = game:GetObjects("rbxassetid://12483505466")[1]
                 obj.Name = "Part"
                 obj.Parent = game:GetService("ReplicatedStorage")
-                --[[for i,v in pairs(obj:GetChildren()) do
+                for i,v in pairs(obj:GetChildren()) do
                     if string.lower(v.Name):find("cross") then
                         for i2,b in pairs(v:GetChildren()) do
                             b:Destroy()
                         end
                     end
-                end--]]
+                end
                 shared.con = game:GetService("ReplicatedStorage").ChildAdded:Connect(function(v)
                     for i,x in pairs(obj:GetChildren()) do
                         x:Clone().Parent = v
