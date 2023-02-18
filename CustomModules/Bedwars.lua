cdb114d27bdb6eb5686e59a6c2fb8d002ea1ea85
EZEZEZEZZE <104246252+EZEZEZEZZE@users.noreply.github.com>
2023-02-18 13:28:21 -0600

    Update Bedwars.lua

--- CustomModules/Bedwars.lua
+++ CustomModules/Bedwars.lua
@@ -2475,6 +2475,146 @@ runcode(function()
 end)
 
 runcode(function()
+    local Enabled = false
+    local Cham = Tabs["Render"]:CreateToggle({
+        ["Name"] = "Chams",
+        ["Callback"] = function(Callback)
+            Enabled = Callback
+            if Enabled then
+                repeat task.wait(5) until game:IsLoaded()
+local c = workspace.CurrentCamera
+local ps = game:GetService("Players")
+local lp = ps.LocalPlayer
+local rs = game:GetService("RunService")
+local function esp(p,cr)
+    local h = cr:WaitForChild("Humanoid")
+    local hrp = cr:WaitForChild("HumanoidRootPart")
+    local text = Drawing.new("Text")
+    text.Visible = false
+    text.Center = true
+    text.Outline = true 
+    text.Font = 2
+    text.Color = Color3.fromRGB(255,255,255)
+    text.Size = 13
+    local c1
+    local c2
+    local c3
+    local function dc()
+        text.Visible = false
+        text:Remove()
+        if c1 then
+            c1:Disconnect()
+            c1 = nil 
+        end
+        if c2 then
+            c2:Disconnect()
+            c2 = nil 
+        end
+        if c3 then
+            c3:Disconnect()
+            c3 = nil 
+        end
+    end
+    c2 = cr.AncestryChanged:Connect(function(_,parent)
+        if not parent then
+            dc()
+        end
+    end)
+    c3 = h.HealthChanged:Connect(function(v)
+        if (v<=0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
+            dc()
+        end
+    end)
+    c1 = rs.RenderStepped:Connect(function()
+        local hrp_pos,hrp_onscreen = c:WorldToViewportPoint(hrp.Position)
+        if hrp_onscreen then
+            text.Position = Vector2.new(hrp_pos.X, hrp_pos.Y)
+            text.Text = p.Name
+            text.Visible = true
+        else
+            text.Visible = false
+        end
+    end)
+end
+local function p_added(p)
+    if p.Character then
+        esp(p,p.Character)
+    end
+    p.CharacterAdded:Connect(function(cr)
+        esp(p,cr)
+    end)
+end
+for i,p in next, ps:GetPlayers() do 
+    if p ~= lp then
+        p_added(p)
+    end
+end
+ps.PlayerAdded:Connect(p_added)
+
+--ESP START
+getgenv().enabled = true --Toggle on/off
+getgenv().uselocalplayer = false --Choose whether the ESP highlights LocalPlayer or not
+getgenv().filluseteamcolor = true --Toggle fill color using player team color on/off
+getgenv().outlineuseteamcolor = false --Toggle outline color using player team color on/off
+getgenv().fillcolor = Color3.new(145, 108, 217) --Change fill color, no need to edit if using team color
+getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
+getgenv().filltrans = 0.25 --Change fill transparency
+getgenv().outlinetrans = 0 --Change outline transparency
+
+local holder = game.CoreGui:FindFirstChild("ESPHolder") or Instance.new("Folder")
+if enabled == false then
+    holder:Destroy()
+end
+if holder.Name == "Folder" then
+    holder.Name = "ESPHolder"
+    holder.Parent = game.CoreGui
+end
+if uselocalplayer == false and holder:FindFirstChild(game.Players.LocalPlayer.Name) then
+    holder:FindFirstChild(game.Players.LocalPlayer.Name):Destroy()
+end
+if getgenv().enabled == true then 
+    getgenv().enabled = false
+    getgenv().enabled = true
+end
+while getgenv().enabled do
+    task.wait()
+    for _,v in pairs(game.Players:GetChildren()) do
+        local chr = v.Character
+        if chr ~= nil then
+        local esp = holder:FindFirstChild(v.Name) or Instance.new("Highlight")
+        esp.Name = v.Name
+        if uselocalplayer == false and esp.Name == game.Players.LocalPlayer.Name then
+            else
+        esp.Parent = holder
+        if filluseteamcolor then
+            esp.FillColor = v.TeamColor.Color
+        else
+            esp.FillColor = fillcolor 
+        end
+        if outlineuseteamcolor then
+            esp.OutlineColor = v.TeamColor.Color
+        else
+            esp.OutlineColor = outlinecolor    
+        end
+        esp.FillTransparency = filltrans
+        esp.OutlineTransparency = outlinetrans
+        esp.Adornee = chr
+        esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+        end
+        end
+    end
+end
+
+            else
+                if getgenv().enabled == true then 
+		getgenv().enabled = false
+          end
+            end
+        end
+    })
+end)
+
+runcode(function()
 	function HasA()
         if IsAlive(lplr) and lplr.Character:FindFirstChild("InventoryFolder").Value:FindFirstChild("hang_glider") then
             return true
